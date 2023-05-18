948e23189bff437d95b5d873fefc5c5f66d45584
VishalCoder78 <vishalcoder78@gmail.com>
2023-05-18 13:55:04 +0530

    first commit

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="flex justify-center items-center h-screen w-screen border">
+    <div class="flex justify-center items-center h-screen">
         <p class="text-gray-400 font-bold text-4xl">Order</p>
     </div>
 </x-app-layout>
